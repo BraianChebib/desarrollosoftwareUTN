77215b6b7be249e952e3393088000bc9182c044f
unknown <lucianochebib@gmail.com>
2026-04-19 23:51:06 -0300

    agrego funciones de calculadora

--- Prueba01/Program.cs
+++ Prueba01/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ProyectoUTN.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calculadora = new CalculadoraService();
+
+        Console.WriteLine(calculadora.Sumar(5, 3));
+        Console.WriteLine(calculadora.Restar(5, 3));
+    }
+}
---
+++ Prueba01/services/CalculadoraService.cs
@@ -0,0 +1,15 @@
+namespace ProyectoUTN.Services
+{
+    public class CalculadoraService
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
